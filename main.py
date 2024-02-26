49ff9fea80ae4fc5e71101a7eec38806f0b1b2c7
m4549071758 <m4549071758@gmail.com>
2024-02-26 19:52:51 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import random
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("logged in as " + client.user.name)
+    print("------")
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        pass
+
+    # メンションされたら実行
+    if client.user in message.mentions:
+        role = message.guild.get_role() #roleID
+        role_members = role.members
+
+        selected_member = random.choice(role_members)
+        selected_member_id = selected_member.id
+
+        await message.channel.send(f"<@{selected_member_id}>さんが選択されました。")
+
+client.run("YOUR_TOKEN")
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Someone Bot
+## Settings
+In `role = message.guild.get_role()`, put the role ID you want to target for the call lottery.
+The ID argument type is INT.
+## Usage
+When you mentions this bot, the bot will mentions a user drawn at random from the roles specified in `role = message.guild.get_role()`.
+## caution
+**DO NOT GIVE THIS BOT A CALL TARGET ROLE**
+
+this supposed to ignore mentions from bots, but just in case.
